7c37c39e8932fe7afc1c1b12430f97041a018a21
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-09-29 20:22:50 +1000

    Add score determining program

---
+++ prac_02/score.py
@@ -0,0 +1,14 @@
+"""
+Program to determine score status
+"""
+
+
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
