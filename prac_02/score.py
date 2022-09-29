eb7349856cfbe63b6f8a4698ffcb2812d678c263
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-09-29 20:43:53 +1000

    Modifying program to generate random score and print score status

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,18 +2,30 @@
 Program to determine score status
 """
 
+from random import randint
+
 
 def main():
     """Get users score and print result"""
+    score = get_valid_score()
+    print(f"Score: {score}, Result: {determine_score_status(score)}")
+    # Using a random integer for score
+    print("Using a random score between 0 to 100, the result will be: ")
+    score = randint(0, 100)
+    print(f"Score: {score}, Result: {determine_score_status(score)}")
+
+
+def get_valid_score():
     score = float(input("Enter score: "))
-    print(determine_score_status(score))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
 
 
 def determine_score_status(score):
     """To determine the status of score"""
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
+    if score >= 90:
         return "Excellent"
     elif score >= 50:
         return "Passable"
