dd4d70a17c319d7213754cf49145e0dcdf0872e4
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-10-08 20:36:41 +1000

    Initial commit

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,26 @@
+"""Lists Warmup"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# The answer of the following expressions are written in the comments
+numbers[0]  # 3
+numbers[-1]  # 2
+numbers[3]  # 1
+numbers[:-1]  # [3, 1, 4, 1, 5, 9]
+numbers[3:4]  # [1]
+5 in numbers  # True
+7 in numbers  # False
+"3" in numbers  # False
+numbers + [6, 5, 3]  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Python expressions to achieve the following:-
+
+
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+# Change the last element of numbers to 1
+numbers[-1] = 1
+# Print all the elements from numbers except the first two (slice)
+print(numbers[2:])
+# Print whether 9 is an element of numbers
+print(9 in numbers)
