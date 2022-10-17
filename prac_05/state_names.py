bc349705ed0c1a78b8c1dddcbe799e78fbf41faf
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-10-17 13:21:21 +1000

    Reformatting the file

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,18 +1,30 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
+File has been formatted
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+# Reformatted this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
+                "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria",
+                "TAS": "Tasmania"}
+
+# CODE_TO_NAME = {}
+
+
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+    try:
+        print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
+        print("Invalid Key Input")
+    state_code = input("Enter short state: ").upper()
+
+if not CODE_TO_NAME:  # To check if the dictionary is empty orn not
+    print("The dictionary is empty.")
+else:
+    for code, name in CODE_TO_NAME.items():
+        print(f"{code:3} is {name}")
