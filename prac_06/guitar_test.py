d259add035f2b26d17e6fb28f3e4f0508dd3405a
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-11-03 00:20:05 +1000

    Adding guitar testing program

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+Guitars Test
+Estimate: 25 minutes
+Actual: 32 minutes
+"""
+
+from prac_06.guitar import Guitar
+
+guitar = Guitar('Gibson L-5 CES', 1922, 16035.40)
+another_guitar = Guitar('Another Guitar', 2013, 999.99)
+
+print(f'{guitar.name} get_age() - Expected 100. Got {guitar.get_age()}')
+print(f'{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}')
+print(f'{guitar.name} is_vintage() - Expected True. Got {guitar.is_vintage()}')
+print(f'{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}'
