45c095b478e65395f4a128cffe5bb1bd2783110c
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-11-09 02:00:18 +1000

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,12 +1,14 @@
 """
 CP1404 Practical
-Guitars
+More Guitars
 """
 
 import csv
 from prac_06.guitar import Guitar
 
-in_file = open('guitars.csv', 'r', newline='')
+FILENAME = "guitars.csv"
+
+in_file = open(FILENAME, 'r', newline='')
 reader = csv.reader(in_file)
 guitars = []
 for row in reader:
@@ -16,3 +18,18 @@ in_file.close()
 guitars.sort()
 for guitar in guitars:
     print(guitar)
+
+print("\nEnter details for new guitar: ")
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    new_guitar = Guitar(name, year, cost)
+    new_guitar_details = [name, year, cost]
+    guitars.append(new_guitar)
+    print(f"{new_guitar} added.")
+    out_file = open(FILENAME, 'a')
+    writer = csv.writer(out_file)
+    writer.writerow(new_guitar_details)
+    out_file.close()
+    name = input("Name: ")
