a9aaed86a3ffbc5beb5d1356781d38b15d0e874c
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2022-11-23 22:04:05 +1000

    Adding SilverServiceTaxi class

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practical
+Silver Service Taxi class
+"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Get the current fare."""
+        return self.flagfall + super().get_fare()
